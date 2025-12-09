ec2e1394d4b7ab48bb4b17d11289441fad326fe7
蒋青江 <2959878253@qq.com>
2025-12-09 17:16:17 +0800

    修改后端地址

--- vue.config.js
+++ vue.config.js
@@ -4,8 +4,7 @@ module.exports = defineConfig({
   devServer: {
     proxy: {
       '/api': {
-        target: 'http://8.137.167.89:8080', // 后端服务器地址
-        // target: 'http://localhost:8080',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         pathRewrite: {
           '^/api': '' // 将 /api 重写为空字符串
